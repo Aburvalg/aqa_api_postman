351bc12a9e2ce5cc915ac1591d23a9a0efa817bd
Aburvalg <3451z@mail.ru>
2022-03-19 13:38:42 +0300

    Initial commit

---
+++ src/test/java/rest/MobileBankApiTestPost.java
@@ -0,0 +1,23 @@
+package rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+
+class MobileBankApiTestPost {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()// Предусловия
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("привет")// отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()// Выполняемые действия
+                .post("/post")
+                .then()// Проверки
+                .statusCode(200)
+                .body("data", equalTo("привет"))
+        ;
+    }
+}

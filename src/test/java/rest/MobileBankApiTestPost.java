73eae7fa9230b724946dd0f97adc0cbf4d9c8540
Aburvalg <3451z@mail.ru>
2022-03-19 13:59:37 +0300

    Test correct

--- src/test/java/rest/MobileBankApiTestPost.java
+++ src/test/java/rest/MobileBankApiTestPost.java
@@ -16,7 +16,7 @@ class MobileBankApiTestPost {
                 .post("/post")
                 .then()// Проверки
                 .statusCode(200)
-                .body("data", equalTo("эй,привет"))
+                .body("data", equalTo("привет"))
         ;
     }
 }
